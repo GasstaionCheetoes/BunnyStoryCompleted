c361b1260f99bc79d1bf1121d7440462aef62217
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-05-15 06:51:04 -0700

    Add project files.

---
+++ BunnyStory.py
@@ -0,0 +1,37 @@
+print("Hello, I have a cute story about a bunny I would like to tell you.")
+print("Before I start my story, I need some information from you.")
+print("After typing your response, please press enter.")
+input("\nPress the enter key to continue....")
+
+breed = input("\nWhat breed is the bunny:  ")
+bunName = input("\nWhat is the bunny's name:   ")
+state = input("\nWhat state do you live in:   ")
+musicGenre = input("\nWhat is your favorite music genre:  ")
+color = input("\nWhat is your favorite color:  ")
+ownerName = input("\nWhat is your name:   ")
+
+print("\nLets begin!")
+
+print("\nThere once was a cute little" , breed , "named" , bunName + ".")
+print(bunName, "really enjoyed the sunsets in" , state + ".")
+print("Tonight", bunName + "' s owner" , ownerName , "was taking him for an evening dirve.")
+print(bunName, "was so excited. It had been a long time since he saw a sunset.")
+
+print("\nWhile enjoying the warm summer breeze coming through the car window," , bunName , "looks up and sees various shades of" , color , "in the sky.")
+
+print(bunName, "is in complete awe of how the", state , "sky looks tonight.")
+print("To top it all off, his favorite type of music" , musicGenre , "was playing softly through the speakers. He was so happy that he began to purr and eventually fell asleep.")
+print("When" , bunName, "woke up, he was back home in bed.")
+print("He was so lucky to see the sky tonight.")
+
+print("\nAfter coming back home", bunName , "thinks to himself that he had not got to fully enjoy the sunset due to falling asleep. He contemplates on running outside to look at the sky some more.")
+print("He decides not to because he doesn't know his way home.", breed, "are the type of bunny's known for being extremeley forgetful.")
+print("Being too afraid to get lost", bunName , "decides to call it a night and get some rest.")
+
+print("\nAfter deciding that he wanted to run outside and see the sky more" , bunName , "did in fact get lost")
+print("Luckily, his owner" , ownerName , "spotted him in the backyard.")
+print(bunName , "was scolded, but was home. Safe and happy.")
+
+
+print("\n")
+
